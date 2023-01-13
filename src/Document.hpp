1bc738763b0fa29d1824dcc87c1d35c64c308065
Jannis Baum <jannis.baum@gmail.com>
2023-01-13 13:49:42 +0100

    feat(#3): calculate nutrition

--- src/Document.hpp
+++ src/Document.hpp
@@ -1,12 +1,19 @@
 #ifndef DOCUMENT_HPP
 #define DOCUMENT_HPP
 
+#include <unordered_map>
 #include <vector>
 
 #include "types.hpp"
 
 class Document {
     std::vector<word_t> words;
+    std::unordered_map<word_t, int> words_tf;
+    int tf_max;
+
+    public:
+        Document(std::vector<word_t> words);
+        dec_t nutrition(word_t word, dec_t c);
 };
 
 #endif
