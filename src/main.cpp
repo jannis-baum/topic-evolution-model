90b9afd7bc9162da65aff8fc8341b3c00834d985
Jannis Baum <jannis.baum@gmail.com>
2023-01-13 14:10:58 +0100

    feat(#3): trying stuff out main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "Document.cpp"
+
+int main(int argc, char* argv[]) {
+    std::vector<int> numbers = {};
+
+    for (int i = 1; i < argc; i++) {
+        numbers.push_back(atoi(argv[i]));
+    }
+
+    Document doc(numbers);
+
+    std::cout << doc << std::endl;
+
+    std::cout << "nutrition for 2: " << doc.nutrition(2, 1) << std::endl;
+}
