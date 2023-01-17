7e6dc0c3c27a2cc1c3c67e6e0417f84fe2c5a0a1
Jannis Baum <jannis.baum@gmail.com>
2023-01-17 17:55:47 +0100

    refactor(#3): for-loop instead of reduce

--- src/CorpusPeriod.cpp
+++ src/CorpusPeriod.cpp
@@ -6,12 +6,9 @@ CorpusPeriod::CorpusPeriod(std::vector<Document> documents)
 : documents(documents) {}
 
 dec_t CorpusPeriod::nutrition(word_t word, dec_t c) {
-    return std::reduce(
-        this->documents.begin(),
-        this->documents.end(),
-        0,
-        [word, c](dec_t total, Document document) {
-            return total + document.nutrition(word, c);
-        }
-    );
+    dec_t total = 0;
+    for (auto &document : this->documents) {
+        total += document.nutrition(word, c);
+    }
+    return total;
 }
