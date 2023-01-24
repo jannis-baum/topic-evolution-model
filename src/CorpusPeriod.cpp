d8aafb95d54a77054a44a574b2488080e8358e60
Jannis Baum <jannis.baum@gmail.com>
2023-01-24 08:32:37 +0100

    fix: remove reduntant check

--- src/CorpusPeriod.cpp
+++ src/CorpusPeriod.cpp
@@ -28,14 +28,10 @@ dec_t CorpusPeriod::termCorrelation(const word_t k, const word_t z) const {
     int n_z = this->nDocumentsContaining({ z });
     int d_t = this->documents.size();
 
-    // check dividents != 0 & log parameter > 0
+    // check dividents != 0
     if (n_k - n_kz == 0) return 0;
     if (d_t - n_z - n_k + n_kz == 0) return 0;
     if (n_z - n_kz == 0) return 0;
-    dec_t logParam = 
-        ((dec_t)n_kz / (n_k - n_kz)) /
-        ((dec_t)(n_z - n_kz) / (d_t - n_z - n_k + n_kz));
-    if (logParam <= 0) return 0;
     if (n_k == 0) return 0;
     if (d_t - n_k == 0) return 0;
 
