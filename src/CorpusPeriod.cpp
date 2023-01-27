d6e0ea1be6912156c85c28fed255670962d4aa61
Jannis Baum <jannis.baum@gmail.com>
2023-01-27 10:28:24 +0100

    feat(#6): create graph nodes

--- src/CorpusPeriod.cpp
+++ src/CorpusPeriod.cpp
@@ -7,6 +7,57 @@
 #include "SemanticGraph.hpp"
 #include "helpers.hpp"
 
+CorpusPeriod::CorpusPeriod(const std::vector<Document> documents, const std::unordered_map<word_t, std::string> &wtostr)
+: documents(documents), wtostr(wtostr), wtonode({}) {
+    // word -> (nDocumentsContaining(word),
+    //          [(all other words, and their co-occurrences with word)])
+    std::unordered_map<word_t, std::pair<int, std::vector<std::pair<word_t, int>>>> occurrences = {};
+    // occurrence for single words
+    std::vector<std::pair<word_t, int>> singleOccurrences = {};
+    for (const auto & [k, _kstr] : this->wtostr) {
+        std::vector<std::pair<word_t, int>> nkzOccurrences = {};
+        for (const auto & [z, _zstr] : this->wtostr) {
+            nkzOccurrences.push_back({ z, this->nDocumentsContaining({ k, z }) });
+        }
+        int nkOccurrences = this->nDocumentsContaining({ k });
+        occurrences[k] = { nkOccurrences, nkzOccurrences };
+        singleOccurrences.push_back({ k, nkOccurrences });
+    }
+
+    // create nodes for words that have 0 < occurrences < Dt / 2
+    for (const auto & [word, occurrence] : singleOccurrences) {
+        if (occurrence && occurrence < this->documents.size() / 2) {
+            this->wtonode.emplace(word, SemanticNode(word, {}));
+        }
+    }
+
+    // sort by number of occurrences for merging
+    std::sort(singleOccurrences.begin(), singleOccurrences.end(), [](const auto & a, const auto & b) {
+        return a.second < b.second;
+    });
+
+    // merge subwords that only occur with another superword are merged by
+    // being represented by the superword's node
+    // (sorting is necessary to correctly resolve relationships with more than
+    // 2 words)
+    for (const auto & [word, occurrence] : singleOccurrences) {
+        if (!this->wtonode.contains(word)) continue;
+
+        auto coOccurrences = occurrences[word].second;
+        for (const auto & [coWord, coOccurrence] : coOccurrences) {
+            if (occurrences[coWord].first == coOccurrence) {
+                // reassign node
+                // operator[] not allowed because SemanticNode has no ()
+                // constructor
+                auto it = this->wtonode.find(coWord);
+                if (it != this->wtonode.end()) {
+                    it->second = this->wtonode.at(word);
+                }
+            }
+        }
+    }
+};
+
 dec_t CorpusPeriod::nutrition(const word_t word, const dec_t c) const {
     dec_t total = 0;
     for (const auto &document : this->documents) {
--- src/CorpusPeriod.hpp
+++ src/CorpusPeriod.hpp
@@ -14,10 +14,11 @@ class CorpusPeriod {
     // reference to Corpus' mapping to resolve word_t (aka int) to string
     const std::unordered_map<word_t, std::string> &wtostr;
     const std::vector<Document> documents;
+    // map words that exist in graph to nodes
+    std::unordered_map<word_t, SemanticNode> wtonode;
 
     public:
-        CorpusPeriod(const std::vector<Document> documents, const std::unordered_map<word_t, std::string> &wtostr)
-        : documents(documents), wtostr(wtostr) {};
+        CorpusPeriod(const std::vector<Document> documents, const std::unordered_map<word_t, std::string> &wtostr);
         // see definitions.md or paper
         dec_t nutrition(const word_t word, const dec_t c) const;
         // number of documents that contain all given words
