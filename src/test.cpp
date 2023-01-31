7e5bcfab95b5a39f5a532f912fda9d504cb8ea2d
Jannis Baum <jannis.baum@gmail.com>
2023-01-31 15:37:06 +0100

    refactor(#6): test suite

--- src/test.cpp
+++ src/test.cpp
@@ -7,29 +7,26 @@
 int testAll() {
     int failedTests = 0;
 
-    failedTests += testCorpusPeriodBasic();
-    failedTests += testCorpusPeriodBasic();
+    std::cout << "CORPUS PERIOD" << std::endl;
+
+    failedTests += genericTest("Subword should point to superword", [](){
+        Corpus corpus = Corpus({{ { "a", "b" }, { "b" }, {}, {}, {} }});
+        return corpus.periods[0].wtonode.at(0).word == corpus.periods[0].wtonode.at(1).word;
+    });
+    failedTests += genericTest("Chain of subwords should point to common superword", [](){
+        Corpus corpus = Corpus({{ {"c", "a", "b" }, { "a", "b" }, { "b" }, {}, {}, {}, {} }});
+        return corpus.periods[0].wtonode.at(0).word == 2
+            && corpus.periods[0].wtonode.at(1).word == 2
+            && corpus.periods[0].wtonode.at(2).word == 2;
+    });
 
     std::cout << "DONE! failed: " << failedTests << std::endl;
     return failedTests;
 }
 
-int testCorpusPeriodBasic() {
-    std::cout << "Subword should point to superword ";
-    // empty vectors to make sure occurrence rate < 50%
-    Corpus corpus = Corpus({{ { "a", "b" }, { "b" }, {}, {}, {} }});
-    bool success = corpus.periods[0].wtonode.at(0).word == corpus.periods[0].wtonode.at(1).word;
-    std::cout << (success ? "✅" : "❌") << std::endl;
-    return !success;
-}
-
-int testCorpusPeriodChain() {
-    std::cout << "Chains of subwords should point to common superword ";
-    Corpus corpus = Corpus({{ {"c", "a", "b" }, { "a", "b" }, { "b" }, {}, {}, {} }});
-
-    bool success = corpus.periods[0].wtonode.at(0).word == 2 
-        && corpus.periods[0].wtonode.at(1).word == 2
-        && corpus.periods[0].wtonode.at(2).word == 2;
-    std::cout << (success ? "✅" : "❌") << std::endl;
+int genericTest(std::string expectation, std::function<bool()> isSuccess) {
+    std::cout << "- " << expectation;
+    bool success = isSuccess();
+    std::cout << " " << (success ? "✅" : "❌") << std::endl;
     return !success;
 }
--- src/test.hpp
+++ src/test.hpp
@@ -1,9 +1,10 @@
 #ifndef TEST_HPP
 #define TEST_HPP
 
-int testAll();
+#include <functional>
+#include <string>
 
-int testCorpusPeriodBasic();
-int testCorpusPeriodChain();
+int testAll();
+int genericTest(std::string expectation, std::function<bool()> isSuccess);
 
 #endif
