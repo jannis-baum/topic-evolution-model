150f61042641209f51ca8ea4b73d5fc8f35111ab
Jannis Baum <jannis.baum@gmail.com>
2023-02-09 11:11:22 +0100

    refactor(tests): test period without corpus

--- src/test/period.cpp
+++ src/test/period.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 #include "tests.hpp"
-#include "../Corpus.hpp"
+#include "../CorpusPeriod.hpp"
 
 int testGraphConstruction() {
     int failedTests = 0;
@@ -9,21 +11,34 @@ int testGraphConstruction() {
     std::cout << std::endl << "GRAPH CONSTRUCTION" << std::endl;
     std::cout << "Node merging" << std::endl;
 
-    failedTests += genericTest("Subword should point to superword", [](){
-        Corpus corpus = Corpus({{ { "a", "b" }, { "b" }, {}, {}, {} }}, 1);
-        return corpus.periods[0].wtonode.at(0).word == corpus.periods[0].wtonode.at(1).word;
+    std::unordered_map<word_t, std::string> mock_wtostr;
+    mock_wtostr[0] = "a";
+    mock_wtostr[1] = "b";
+    mock_wtostr[2] = "c";
+
+    failedTests += genericTest("Subword should point to superword", [&mock_wtostr](){
+        CorpusPeriod cp = CorpusPeriod({
+            // empty docs to avoid ignoring words for occuring too many times
+            { 0, 1 }, { 1 }, {}, {}, {}
+        }, mock_wtostr, 1);
+        return cp.wtonode.at(0).word == cp.wtonode.at(1).word;
     });
-    failedTests += genericTest("Chain of subwords should point to common superword", [](){
-        Corpus corpus = Corpus({{ {"c", "a", "b" }, { "a", "b" }, { "b" }, {}, {}, {}, {} }}, 1);
-        return corpus.periods[0].wtonode.at(0).word == 2
-            && corpus.periods[0].wtonode.at(1).word == 2
-            && corpus.periods[0].wtonode.at(2).word == 2;
+
+    failedTests += genericTest("Chain of subwords should point to common superword", [&mock_wtostr](){
+        CorpusPeriod cp = CorpusPeriod({
+                { 2, 0, 1 }, { 0, 1 }, { 1 }, {}, {}, {}, {}
+        }, mock_wtostr, 1);
+        return cp.wtonode.at(0).word == 1
+            && cp.wtonode.at(1).word == 1
+            && cp.wtonode.at(2).word == 1;
     });
 
     std::cout << "Edge building" << std::endl;
 
-    failedTests += genericTest("Edges are built without error", [](){
-        Corpus corpus = Corpus({{ {"c", "a", "b" }, { "a", "b" }, { "c", "a" }, { "b", "c" }, {"c", "b"}, {"c", "b"}, {}, {}, {}, {}, {}, {}, {} }}, 1);
+    failedTests += genericTest("Edges are built without error", [&mock_wtostr](){
+        CorpusPeriod cp = CorpusPeriod({
+                { 2, 0, 1 }, { 0, 1 }, { 2, 0 }, { 1, 2 }, { 2, 1 }, { 2, 1 }, {}, {}, {}, {}, {}, {}, {} 
+        }, mock_wtostr, 1);
         return true;
     });
 
