e3b5b7c4c71c093605451d7c5c07bcc709036163
Jannis Baum <jannis.baum@gmail.com>
2023-02-14 10:11:06 +0100

    test(#31): address comment

--- src/test/corpus.cpp
+++ src/test/corpus.cpp
@@ -26,22 +26,18 @@ class MockCorpus: public Corpus {
         MockCorpus(const int testingCase): Corpus() {
             switch (testingCase) {
                 case 4:
-                    {std::vector<Topic> topics = {};
                     this->mock_wtonode.emplace(0, SemanticNode(0, {}));
                     this->mock_wtonode.emplace(1, SemanticNode(1, {}));
                     this->mock_wtonode.emplace(2, SemanticNode(2, {}));
                     this->mock_wtonode.emplace(3, SemanticNode(3, {}));
                     this->mock_wtonode.emplace(4, SemanticNode(4, {}));
                     this->mock_wtonode.emplace(5, SemanticNode(5, {}));
-                    Topic t = {&(this->mock_wtonode.at(0)), &(this->mock_wtonode.at(1))};
-                    topics.push_back(t);
-                    t = {&(this->mock_wtonode.at(0)), &(this->mock_wtonode.at(1)), &(this->mock_wtonode.at(3)), &(this->mock_wtonode.at(5))};
-                    topics.push_back(t);
-                    t = {&(this->mock_wtonode.at(3)), &(this->mock_wtonode.at(4)),  &(this->mock_wtonode.at(2))};
-                    topics.push_back(t);
 
-                    this->topicbyperiod.push_back(topics);
-                    }
+                    this->topicbyperiod.push_back({
+                        Topic({ &(this->mock_wtonode.at(0)), &(this->mock_wtonode.at(1)) }),
+                        Topic({ &(this->mock_wtonode.at(0)), &(this->mock_wtonode.at(1)), &(this->mock_wtonode.at(3)), &(this->mock_wtonode.at(5)) }),
+                        Topic({ &(this->mock_wtonode.at(3)), &(this->mock_wtonode.at(4)), &(this->mock_wtonode.at(2)) })
+                    });
                 case 3:
                     // 0 -> 1 -> 2 -> 3 -> 0
                     this->mock_wtonode.emplace(0, SemanticNode(0, {}));
