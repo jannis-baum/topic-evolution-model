8ebad3957dae7cc39d2bb0cb1a696f0795122181
Jannis Baum <jannis.baum@gmail.com>
2023-02-19 21:38:37 +0100

    feat(#33): dump tuples

--- src/test/yaml.cpp
+++ src/test/yaml.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
 #include "../yaml.hpp"
 #include "tests.hpp"
@@ -10,12 +11,18 @@ int testYaml() {
     std::cout << std::endl << "YAML" << std::endl;
 
     failedTests += genericTest("Correctly dumps arrays of dictionaries", []() {
-        std::vector<int> data = { 1, 2 };
-        auto dump = dumpVector<int>(data, [](const int &element) {
+        const std::vector<int> data = { 1, 2 };
+        const auto dump = dumpVector<int>(data, [](const int &element) {
             return "first: " + std::to_string(element) +
                    "\nsecond: " + std::to_string(element) + "\n";
         });
         return dump == "- first: 1\n  second: 1\n- first: 2\n  second: 2\n";
+    });
+
+    failedTests += genericTest("Correctly dumps tuples", []() {
+        const auto data = std::make_tuple(1, 2);
+        auto dump = dumpTuple(data, { "label1", "label2" });
+        return dump == "label1: 1\nlabel2: 2\n";
     });
 
     return failedTests;
--- src/yaml.hpp
+++ src/yaml.hpp
@@ -10,9 +10,10 @@
 #include "topics.hpp"
 
 // couldn't get the implicit template instantiation to compile if i put the
-// definition in a source file so now it's here - jannis
+// definition in a source file so now they're here - jannis
+
 template<typename T>
-std::string dumpVector(std::vector<T> &data, std::function<std::string(const T &)> dumpElement) {
+std::string dumpVector(const std::vector<T> &data, const std::function<std::string(const T &)> dumpElement) {
     std::string dump = "";
 
     for (const auto &element : data) {
@@ -25,6 +26,17 @@ std::string dumpVector(std::vector<T> &data, std::function<std::string(const T &
         }
     }
 
+    return dump;
+}
+
+template <typename... Tp>
+std::string dumpTuple(const std::tuple<Tp...> &data, const std::vector<std::string> &labels) {
+    std::string dump = "";
+    std::apply([&dump, &labels](auto const &...elements) { 
+      int i = 0; 
+      // variadic fold expression to (compile time) "iterate" over tuple
+      ((dump += labels[i++] + ": " + std::to_string(elements) + "\n"), ...);
+    }, data);
     return dump;
 }
 
