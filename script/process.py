cacb1c938c48b4a4ff12b90575fea917e29ef2fe
Jannis Baum <jannis.baum@gmail.com>
2023-12-13 21:23:18 +0100

    refactor(#48): rely on preprocessing

--- script/process.py
+++ script/process.py
@@ -3,8 +3,6 @@ import os
 
 import pexpect
 
-from nlp import get_structured_corpus
-
 _te_exec = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
     '.build.out', 'out'
@@ -15,9 +13,7 @@ def _init_worker(getter, args: list[str]):
     # process
     getter.proc = pexpect.spawn(_te_exec, args, echo=False)
 
-def _get_output(text: str) -> str:
-    corpus = get_structured_corpus(text)
-
+def _get_output(corpus: str) -> str:
     _get_output.proc.sendline(corpus + '\0')
     _get_output.proc.expect('\0')
 
@@ -51,5 +47,7 @@ class TEM:
         if metrics: args.append('--metrics')
         self.pool = Pool(initializer=_init_worker, initargs=(_get_output, args))
 
-    def get_outputs(self, texts: list[str]) -> list[str]:
-        return self.pool.map(_get_output, texts)
+    # get model outputs for structured corpora
+    # create corpora with nlp.get_structured_corpus
+    def get_outputs(self, corpora: list[str]) -> list[str]:
+        return self.pool.map(_get_output, corpora)
