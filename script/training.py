2d6ff9cc598dea3e3c4d5cbfcb689605476dcd55
Jannis Baum <jannis.baum@gmail.com>
2023-12-14 18:44:37 +0100

    feat(#48): gradient descent boundaries

--- script/training.py
+++ script/training.py
@@ -9,3 +9,14 @@ def metrics(corpora: list[str], params: npt.NDArray[np.float64]) -> npt.NDArray[
         merge_threshold=params[5], evolution_threshold=params[6]
     )
     return np.array([eval(metrics) for metrics in model.get_outputs(corpora)])
+
+def gradient_descent(
+    # params: c, alpha, beta, gamma, delta, merge_threshold, evolution_threshold
+    initial_params: npt.NDArray[np.float64] = np.array([0.5, 0, -1, 0, 1, 100, 100]),
+    # boundaries: (lower, upper) limit for params
+    boundaries: tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]] = (
+        np.array([0, -3, -3, -3, -3, 0, 100]),
+        np.array([1,  3,  3,  3,  3, 0, 100])
+    ),
+) -> tuple[float, np.ndarray]:
+    return (0, initial_params)
