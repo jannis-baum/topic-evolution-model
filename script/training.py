42dfef840129b9e9d443ae52dc6b38fb925a27bc
Jannis Baum <jannis.baum@gmail.com>
2023-12-17 20:12:01 +0100

    feat(#48): gradient descent

--- script/training.py
+++ script/training.py
@@ -15,7 +15,7 @@ def metrics(corpora: list[str], params: npt.NDArray[np.float64]) -> npt.NDArray[
 def gradient_descent(
     # TEM-ready corpora
     corpora: list[str],
-    # cost function to find local minimum of: metrics-matrix -> cost
+    # cost function to find local minimum of: metrics-matrix -> cost >= 0
     cost: Callable[[npt.NDArray[np.float64]], np.float64],
 
     # params: c, alpha, beta, gamma, delta, merge_threshold, evolution_threshold
@@ -30,9 +30,32 @@ def gradient_descent(
     delta: np.float64 = np.float64(0.01),
     # learning rate
     rate: np.float64 = np.float64(0.1),
-    # stop criterion: if cost makes relative improvement smaller than this, exit
+    # stop criterion: if cost makes change smaller than this, exit
     epsilon: np.float64 = np.float64(0.01),
     # stop criterion: stop after this many iterations
     max_iter: int = 50,
 ) -> tuple[np.float64, npt.NDArray[np.float64]]:
-    return (np.float64(0), initial_params)
+
+    params = initial_params
+    def evaluate_cost(adjust: tuple[int, np.float64] | None = None):
+        p = params
+        if adjust: p[adjust[0]] = adjust[1]
+        return cost(metrics(corpora, p))
+
+    # ACTUAL GRADIENT DESCENT
+    rate = rate / delta # step adjusted learning rate
+    current_cost = np.float64(-100) # fine as initial value because cost is always positive
+    for _ in range(max_iter):
+        # update cost & check if we have converged
+        old_cost = current_cost
+        current_cost = evaluate_cost()
+        if abs(old_cost - current_cost) < epsilon: break
+        # UPDATE PARAMETERS ALONG GRADIENT
+        # we check the difference a small step (delta) would make, scale it by
+        # 1/delta to get the gradient, multiply with the learning rate and
+        # finally add it to the current value -> step downwards
+        params = np.array([
+            rate * (current_cost - evaluate_cost((i, param + delta)))
+        for i, param in enumerate(params)])
+
+    return (current_cost, params)
