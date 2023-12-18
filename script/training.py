17879b71131f5f02556ba44e2e556033a079c381
Jannis Baum <jannis.baum@gmail.com>
2023-12-18 13:55:28 +0100

    refactor(#48): cost -> loss

--- script/training.py
+++ script/training.py
@@ -15,8 +15,8 @@ def metrics(corpora: list[str], params: npt.NDArray[np.float64]) -> npt.NDArray[
 def gradient_descent(
     # TEM-ready corpora
     corpora: list[str],
-    # cost function to find local minimum of: metrics-matrix -> cost >= 0
-    cost: Callable[[npt.NDArray[np.float64]], np.float64],
+    # loss function to find local minimum of: metrics-matrix -> loss >= 0
+    loss: Callable[[npt.NDArray[np.float64]], np.float64],
 
     # params: c, alpha, beta, gamma, delta, merge_threshold, evolution_threshold
     initial_params: npt.NDArray[np.float64] = np.array([0.5, 0, -1, 0, 1, 50, 50]),
@@ -30,38 +30,38 @@ def gradient_descent(
     delta: np.float64 = np.float64(0.01),
     # learning rate
     rate: np.float64 = np.float64(0.1),
-    # stop criterion: if cost makes change smaller than this, exit
+    # stop criterion: if loss makes change smaller than this, exit
     epsilon: np.float64 = np.float64(0.01),
     # stop criterion: stop after this many iterations
     max_iter: int = 50,
 ) -> tuple[np.float64, npt.NDArray[np.float64]]:
 
     params = initial_params
-    def evaluate_cost(adjust: tuple[int, np.float64] | None = None):
+    def evaluate_loss(adjust: tuple[int, np.float64] | None = None):
         p = np.copy(params)
         if adjust: p[adjust[0]] = adjust[1]
-        return cost(metrics(corpora, p))
+        return loss(metrics(corpora, p))
 
     def constrain_boundaries(index: int, value: np.float64) -> np.float64:
         return min(boundaries[1][index], max(boundaries[0][index], value))
 
     # ACTUAL GRADIENT DESCENT
     rate = rate / delta # step adjusted learning rate
-    current_cost = np.float64(-100) # fine as initial value because cost is always positive
+    current_loss = np.float64(-100) # fine as initial value because loss is always positive
     for _ in range(max_iter):
-        # update cost & check if we have converged
-        old_cost = current_cost
-        current_cost = evaluate_cost()
-        if abs(old_cost - current_cost) < epsilon: break
+        # update loss & check if we have converged
+        old_loss = current_loss
+        current_loss = evaluate_loss()
+        if abs(old_loss - current_loss) < epsilon: break
         # UPDATE PARAMETERS ALONG GRADIENT
         # we check the difference a small step (delta) would make, scale it by
         # 1/delta to get the gradient, multiply with the learning rate and
         # finally add it to the current value -> step downwards
         params = np.array([
-            constrain_boundaries(i, param + rate * (current_cost - evaluate_cost((i, param + delta))))
+            constrain_boundaries(i, param + rate * (current_loss - evaluate_loss((i, param + delta))))
         for i, param in enumerate(params)])
 
-    return (current_cost, params)
+    return (current_loss, params)
 
 # loss for binary classification problem
 # evaluates contrastive loss between classes so that first class is
