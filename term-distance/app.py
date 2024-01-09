f90d1e982f555f3c68fdac6b89b256244766123b
Jannis Baum <jannis.baum@gmail.com>
2024-01-09 17:09:06 +0100

    feat(#54): similarity endpoint

--- term-distance/app.py
+++ term-distance/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+import gensim.downloader as api
 
 app = Flask(__name__)
+wv = api.load('word2vec-google-news-300')
 
-
-@app.route('/')
+@app.route('/similarity')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    args = request.args
+    if 'a' not in args or 'b' not in args:
+        return 'Words a or b missing in arguments'
+    return str(wv.similarity(args['a'], args['b']))
