1d18bac6e21ba1410b0df6b9599449339b2bad6a
Jannis Baum <jannis.baum@gmail.com>
2024-02-06 19:15:31 +0100

    feat(#54): batch similarity

--- term-distance/app.py
+++ term-distance/app.py
@@ -4,9 +4,15 @@ import gensim.downloader as api
 app = Flask(__name__)
 wv = api.load('word2vec-google-news-300')
 
-@app.route('/similarity')
+@app.route('/similarity', methods=['POST'])
 def hello():
-    args = request.args
-    if 'a' not in args or 'b' not in args:
-        return 'Words a or b missing in arguments'
-    return str(wv.similarity(args['a'], args['b']))
+    body = request.get_data().decode()
+    words = body.split('\n')
+    n = len(words)
+    sims = [['0'] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            dist = str(wv.similarity(words[i], words[j]))
+            sims[i][j] = dist
+            sims[j][i] = dist
+    return '\n'.join((' '.join(row) for row in sims))
