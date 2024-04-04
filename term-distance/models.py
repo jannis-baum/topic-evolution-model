7f7cc264ba13c453c11912cb248ea91d6cc602f1
Jannis Baum <jannis.baum@gmail.com>
2024-04-04 08:42:47 +0200

    feat(#85): download all needed models

--- term-distance/models.py
+++ term-distance/models.py
@@ -1,3 +1,10 @@
+import gensim.downloader as api
+
 lang2model = {
     'en': 'word2vec-google-news-300'
 }
+
+if __name__ == '__main__':
+    for language, model in lang2model.items():
+        print(f'Downloading "{model}" for language "{language}"')
+        api.load(model, return_path=True)
